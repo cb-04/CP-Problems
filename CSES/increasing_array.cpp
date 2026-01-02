87c3908750b0674f78207686b1a893a14a0cb6d1
Chandrabali Biswas <chandrabalibiswas04@gmail.com>
2026-01-02 19:15:04 +0530

    cses increasing array solved

---
+++ CSES/increasing_array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    long long soln = 0;
+    for(int i=1;i<n;i++){
+        if(arr[i]<arr[i-1]){
+            soln += arr[i-1]-arr[i];
+            arr[i] = arr[i-1];
+        }
+    }
+    cout<<soln<<'\n';
+    return 0;
+}
